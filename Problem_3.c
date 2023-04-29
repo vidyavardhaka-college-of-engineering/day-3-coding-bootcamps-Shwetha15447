c3347f38b8badde86871fb5b31ef1c23c68f85dc
Shwetha s <79088799+Shwetha15447@users.noreply.github.com>
2023-04-29 15:18:58 +0530

    Update Problem_3.c

--- Problem_3.c
+++ Problem_3.c
@@ -5,8 +5,14 @@ int main() {
     int number;
     printf("Enter an integer: ");
     scanf("%d", &number);
-
-    // Write your code here
+    if(number%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
     
     
    
