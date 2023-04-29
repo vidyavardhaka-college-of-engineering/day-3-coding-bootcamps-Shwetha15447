17d18d928a9c54d0ddece49704c9a6adb9f09381
Shwetha s <79088799+Shwetha15447@users.noreply.github.com>
2023-04-29 15:10:29 +0530

    Update Problem_4.c

--- Problem_4.c
+++ Problem_4.c
@@ -9,7 +9,13 @@ int main()
 
   	scanf("%d", &n);
 
-  	//Write your code here
+  	for(int i=0;i<n;i++)
+    {
+      for(int j=0;j<=i;j++)
+      {
+        printf("*");
+      }
+    }
   	
 
   	return 0;
