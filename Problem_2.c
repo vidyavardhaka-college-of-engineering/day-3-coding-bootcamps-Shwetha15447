f6302f2ea6be03f6b9bb629adf489d7ab9eec43d
Shwetha s <79088799+Shwetha15447@users.noreply.github.com>
2023-04-29 15:17:54 +0530

    Update Problem_2.c

--- Problem_2.c
+++ Problem_2.c
@@ -10,8 +10,17 @@ int main()
     int n;
     printf("Enter the value for n\n");
     scanf("%d", &n);
-
-    //Write your code here
+    int n1=0;
+    int n2=1;
+    printf("%d",n1);
+    printf("%d",n2);
+    for(int i=2;i<n;i++)
+    {
+        int n3=n1+n2;
+        printf("%d",n3);
+        n1=n2;
+        n2=n3;
+    }    
     
 
     return 0;
